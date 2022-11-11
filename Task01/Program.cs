41d8c2a5b5ffabe0b25832147a6d89a62974f84a
immortalstr <immortalstr1990@mail.ru>
2022-11-11 11:29:07 +0300

    Add first Programm

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программуб которая:
+// 1. на вход принимает число 
+// 2. и выдает его квадрат
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// int number = int.Parse(Console.ReadLine());  --- преобразует только строку в int
+
+
+int square = number * number;
+
+Console.WriteLine($"Квадрат числа {number} равен {square}");
+
+// Для интерполяции строки используется ($"Квадрат числа {number}")
