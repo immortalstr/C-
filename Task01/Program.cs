5d2d52acb314d107887a849f7e79257328e0dd78
immortalstr <immortalstr1990@mail.ru>
2022-11-11 12:04:02 +0300

    Add to Push1

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -24,11 +24,11 @@ Console.WriteLine("Введите первое число");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int b = Convert.ToInt32(Console.ReadLine());
-if (a == b*b) 
+if (a == b * b)
 {
     Console.WriteLine($"Число {a} является квадратом {b}");
 }
-else 
+else
 {
     Console.WriteLine($"Число {a} не является квадратом {b}");
 }
