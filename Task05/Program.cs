9dd54675cb398e62a2f7c42329e3be02fa083865
immortalstr <immortalstr1990@mail.ru>
2022-11-15 11:55:05 +0300

    ned
    Add Task05

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -26,13 +26,13 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число 2");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-bool Multiplicity (int number1, int number2)
+bool Multiplicity(int number1, int number2)
 {
-     return number1 % number2 == 0;
+    return number1 % number2 == 0;
 }
 
-int MultiplicitySecond (int number1, int number2)
-{ 
+int MultiplicitySecond(int number1, int number2)
+{
     return number1 % number2;
 }
 
@@ -40,4 +40,4 @@ int MultiplicitySecond (int number1, int number2)
 // Console.WriteLine(result ? "Первое число кратно второму" : $"Некратно, остаток = {num1 % num2}" );
 
 int result2 = MultiplicitySecond(num1, num2);
-Console.WriteLine(result2 == 0 ? "Первое число кратно второму" : $"Некратно, остаток = {result2}" );
+Console.WriteLine(result2 == 0 ? "Первое число кратно второму" : $"Некратно, остаток = {result2}");
