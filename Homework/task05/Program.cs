58c645b733bcaaf8986aa23f67bccf46c51a558e
immortalstr <immortalstr1990@mail.ru>
2022-11-15 14:18:00 +0300

    Finish task06 task06 task07

--- Homework/task05/Program.cs
+++ Homework/task05/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Вторая цифра числа {num} равна: ({SecondNumber(num)})");
+
+int SecondNumber(int number)
+{
+    return (number % 100 / 10);
+}
