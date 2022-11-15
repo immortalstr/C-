abc2cdd90f85e9514585e8888907a5a11575164f
immortalstr <immortalstr1990@mail.ru>
2022-11-15 11:12:24 +0300

    Add Task 04

---
+++ Task04/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайная цифра из отрезка == {number}");
+
+int KillsecondDigit(int num)
+{
+    int firstDigit = num / 100;
+    int threedDigit = num % 10;
+
+    int result = firstDigit * 10 + threedDigit;
+    return result;
+}
+
+Console.WriteLine(KillsecondDigit(number));
