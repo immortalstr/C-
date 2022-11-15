d49a0e8741f08a68cae44a43da68eddb3fb71d1a
immortalstr <immortalstr1990@mail.ru>
2022-11-15 14:19:34 +0300

    Finish task06 task06 task07

--- Homework/task06/Program.cs
+++ Homework/task06/Program.cs
@@ -5,3 +5,19 @@
 // 78 -> третьей цифры нет
 
 // 32679 -> 6
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(num > 100 ? $"Третья цифра это - {ThirdNumber(num) % 10} " : "Третьей цифры нет");
+
+int ThirdNumber(int number)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+
+    }
+    return number;
+}
--- Homework/task07/Program.cs
+++ Homework/task07/Program.cs
@@ -3,3 +3,15 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(DayofTheWeek(number) ? "Да" : "Нет");
+
+bool DayofTheWeek(int num)
+{
+    return (num == 6) || (num == 7);
+}
