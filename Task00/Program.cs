79d5d9a38e0be9c99cd5c33748338a039ccae74e
immortalstr <immortalstr1990@mail.ru>
2022-11-15 10:57:25 +0300

    add Task00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,8 +1,24 @@
-﻿int number = new Random().Next();
+﻿Console.Clear();
+
+int number = new Random().Next(10, 100);
 Console.WriteLine(number);
 
-int firstDigit = number / 10;
-int secondDigit = number % 10;
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
 
-if (firstDigit > secondDigit) Console.WriteLine(firstDigit);
-else Console.WriteLine(secondDigit);
+// if (firstDigit > secondDigit) Console.WriteLine(firstDigit);
+// else Console.WriteLine(secondDigit);
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    // if (firstDigit > secondDigit) return firstDigit;
+    // else return secondDigit;
+
+    // int max = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа --> {maxDigit}");
