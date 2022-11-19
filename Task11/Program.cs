1948c720af85b8faca28b80ff6aa499361cbdeab
immortalstr <immortalstr1990@mail.ru>
2022-11-19 17:56:03 +0300

    Finish Task 11

---
+++ Task11/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int number = 1;
+
+void Square(int n)
+{
+while (number < num + 1)
+    {
+        Console.WriteLine($"{number} --> {number * number}");
+        number++;
+    }
+}
+Console.WriteLine($"Квадраты чисел числа {num} равно: ");
+Square(num);
