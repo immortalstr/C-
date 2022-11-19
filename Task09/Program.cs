3da7bd92fbeecce58fd5c59e1882acd64b31a561
immortalstr <immortalstr1990@mail.ru>
2022-11-19 16:33:50 +0300

    Task 09 finish

---
+++ Task09/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Укажите номер четверти: ");
+string quarter = Console.ReadLine();
+string Range(string quarter1)
+{
+    if (quarter1 == "1") return "x › 0 и y > 0";
+    if (quarter1 == "2") return "x < 0 и y > 0";
+    if (quarter1 == "3") return "x < 0 и y < 0";
+    if (quarter1 == "4") return "x > 0 и y < 0";
+    return "Вася ты ввел не те данные ВАСЯ!!!";
+}
+string range = Range(quarter);
+Console.WriteLine(range);
