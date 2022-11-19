2fb580d7059e2c04886ede24c0b466e3353e3b3d
immortalstr <immortalstr1990@mail.ru>
2022-11-20 01:10:15 +0300

    Add task08 and finish

--- Homework/task08/Program.cs
+++ Homework/task08/Program.cs
@@ -9,23 +9,25 @@
 
 // 23432 -> да
 
-Console.Clear();
 Console.WriteLine("Введите пятизначное число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 
 int num2 = 0;
 
 int Rev(int numb)
-{ 
+{
     while (numb != 0)
     {
         num2 = (num2 + (numb % 10)) * 10;
-        numb = numb /10;
-        
+        numb = numb / 10;
+
     }
     return num2 / 10;
 }
 
-if (Rev(num1) == num1) Console.WriteLine($"{num1} --> ДА!!");
-else Console.WriteLine($"{num1} --> НЕТ!!");
-
+if (num1 >= -99999 && num1 <= 99999)
+{
+    if (Rev(num1) == num1) Console.WriteLine($"{num1} --> ДА!!");
+    else Console.WriteLine($"{num1} --> НЕТ!!");
+}
+else Console.WriteLine("Введите пятизначное число:");
