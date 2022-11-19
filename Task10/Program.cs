495ffec45e670545f2c30bd94d2180ca4dffe389
immortalstr <immortalstr1990@mail.ru>
2022-11-19 17:18:35 +0300

    Finish Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите точку ХА: ");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите точку YA: ");
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите точку ХВ: ");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите точку УВ: ");
+int yb = Convert.ToInt32(Console.ReadLine());
+
+double Distance(int xa, int ya, int xb, int yb)
+{
+    double xc = (xa - xb) * (xa - xb);
+    double yc = (ya - yb) * (ya - yb);
+    double result = Math.Sqrt(xc + yc);
+    return result;
+}
+
+double res = Distance(xa, ya, xb, yb);
+double resRound = Math.Round(res, 2, MidpointRounding.ToZero);
+
+Console.WriteLine(resRound);
