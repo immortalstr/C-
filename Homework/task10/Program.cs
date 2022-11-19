437266105a7f4e7e71448978609d08af8765b14a
immortalstr <immortalstr1990@mail.ru>
2022-11-20 02:00:06 +0300

    Add task10  and finish

---
+++ Homework/task10/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+void Res(int number)
+{
+    while (count <= num)
+    {
+        Console.WriteLine($"{count} | {count * count * count}");
+        count++;
+    }
+}
+
+Res(num);
