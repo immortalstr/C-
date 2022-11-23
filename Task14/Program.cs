00d5b152f33a4d7fff237d4b62807bef38e6f562
immortalstr <immortalstr1990@mail.ru>
2022-11-23 14:43:48 +0300

    Add and finish Classwork Task14 and Task15

---
+++ Task14/Program.cs
@@ -0,0 +1,25 @@
+﻿// int[] array = new int[8];
+// int[] array1 = { 1, 2, 3, 5, 7, 1, 2 };
+// int[] array2 = new int[4] { 1, 2, 3, 4 };
+
+int[] array = new int[8];
+
+void RandomArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 2);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+RandomArray(array);
+PrintArray(array);
---
+++ Task15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите Число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Count(int num)
+{
+    int counter = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        counter = counter * i;
+    }
+    return counter;
+}
+
+int count = Count(number);
+
+Console.WriteLine($"{number} --> {count}");
