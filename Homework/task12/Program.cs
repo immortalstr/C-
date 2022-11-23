9c69a612932fa8655f53b9189e3f89f85c9d293a
immortalstr <immortalstr1990@mail.ru>
2022-11-23 16:16:26 +0300

    Add and finish Homework task12

--- Homework/task12/Program.cs
+++ Homework/task12/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Count(int number)
+{
+    int counter = 0;
+    if (number < 0) number = number * -1;
+    while (number > 0)
+    {
+        counter = counter + number % 10;
+        number = number / 10;
+    }
+    return counter;
+}
+
+Console.WriteLine($"{num} --> {Count(num)}");
