95411c38625e2aa0da152a595d2382fe85ba0a9c
immortalstr <immortalstr1990@mail.ru>
2022-11-23 13:11:44 +0300

    Add and finish Classwork Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите Число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Count(int num)
+{
+    int counter = 0;
+    for (int i = 0; i <= num; i++)
+    {
+        counter = counter + i;
+    }
+    return counter;
+}
+
+Console.WriteLine($"{number} --> {Count(number)}");
