ff8d2ebb126b81bb1fb2b5d362a8359a81d8f4db
immortalstr <immortalstr1990@mail.ru>
2022-11-26 13:54:06 +0300

    Add Task21 and finish

---
+++ Task21/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Clear();
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+
+    }
+    Console.WriteLine("]");
+}
+
+int[] array = CreateArrayRndInt(12, -9, 0);
+PrintArray(array);
+
+bool FindNumberArray(int[] arr, int numb)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == numb) return true;
+    }
+    return false;
+}
+
+Console.WriteLine("Введите любое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("заданное число присутствует в массиве? ");
+Console.WriteLine(FindNumberArray(array, number)? "yes" : "no");
