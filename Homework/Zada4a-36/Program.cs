2c51bc8ff5365409162e9b4785fc99464ec90a04
immortalstr <immortalstr1990@mail.ru>
2022-11-27 18:54:31 +0300

    Add and finish Zada4a-36

---
+++ Homework/Zada4a-36/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+Console.WriteLine();
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+
+    }
+    Console.Write("] Сумма Элементов стоящих на нечетных позициях =  ");
+}
+
+
+int SumNegativePosition(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i % 2 != 0) count = count + arr[i];
+    }
+    return count;
+}
+
+
+int[] array = CreateArrayRndInt(6, -10, 10);
+
+int counter = SumNegativePosition(array);
+PrintArray(array);
+Console.WriteLine($"{counter}");
