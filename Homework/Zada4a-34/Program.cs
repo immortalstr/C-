5db5193d1231ec96bd69c50c875822f982d8c60d
immortalstr <immortalstr1990@mail.ru>
2022-11-27 18:21:40 +0300

    Add and finish Zada4a-34

---
+++ Homework/Zada4a-34/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine();
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+
+    }
+    Console.Write("] Колличество 'четных' чисел в массиве = ");
+}
+
+
+int CountPositiveEllements(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 2 == 0) count = count + 1;
+    }
+    return count;
+}
+
+
+int[] array = CreateArrayRndInt(10, 100, 1000);
+
+int counter = CountPositiveEllements(array);
+PrintArray(array);
+Console.WriteLine($"{counter}");
