48c186967301b96222bfdd7cd12e054789a40fd7
immortalstr <immortalstr1990@mail.ru>
2022-11-30 17:04:40 +0300

    Add and Finish Zada4a41

---
+++ Homework/Zada4a-41/Program.cs
@@ -0,0 +1,62 @@
+﻿Console.Clear();
+void PrintArray(int[] array)
+{
+    Console.Write($"[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine($"]");
+}
+// int [] ReadNumbers(int size)
+// {
+//     int[] array2 = new int[size];
+//     int i = 0;
+//     while (i < size)
+//     {
+//         Console.WriteLine($"Ввведите число");
+//         array2[i] = Convert.ToInt32(Console.ReadLine());
+//         i++;
+//     }
+//     return array2;
+// }
+int PositiveNumbers(int [] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]  > 0) counter++;
+    }
+    return counter;
+}
+
+// Console.WriteLine($"Ввведите колличество числе которые собираетесь вводить");
+// int length = Convert.ToInt32(Console.ReadLine());
+// int [] array1 = ReadNumbers(length);
+
+Console.WriteLine($"Ввведите числа через запятую, например 0, 7, 8, -2, -2 ");
+string numbers = Console.ReadLine();
+
+string[] Separation(string num)
+{
+    string[] spearator = {", "};
+    string[] strlist = num.Split(spearator, StringSplitOptions.RemoveEmptyEntries);
+    return strlist;
+}
+int [] ConVertation(string [] strlist)
+{
+    int[] convertedarray = new int[strlist.Length];
+    for (int i = 0; i < strlist.Length; i++)
+    {
+        convertedarray[i] = Convert.ToInt32(strlist[i]);
+    }
+    return convertedarray;
+}
+string [] StrNumbers = Separation(numbers);
+int[] array1 = ConVertation(StrNumbers);
+// int[] array1 = Array.ConvertAll(StrNumbers, s => int.Parse(s));
+Separation(numbers);
+Console.WriteLine($"Числа преобразованны в массив");
+PrintArray(array1);
+Console.WriteLine($"Колличество чисел больше нуля = {PositiveNumbers(array1)}");
---
+++ Task24/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Clear();
+
+Console.WriteLine("");
+Console.WriteLine("");
+
+// int num = Convert.ToInt32(Console.ReadLine());
+
+
+int[] CreateArrayInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArrayInt(int[] arr, string str1, string str2)
+{
+    Console.Write($"{str1}");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine($"{str2}");
+}
+
+int[] array = CreateArrayInt(5, 1, 10);
+PrintArrayInt(array, "[", "]");
+
+int[] CreateReverseArray(int[] arr)
+{
+    int[] newArray = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        newArray[i] = arr[arr.Length -1 - i];
+    }
+    return newArray;
+}
+
+int[] neaArr = CreateReverseArray(array);
+PrintArrayInt(neaArr, "[", "]");
