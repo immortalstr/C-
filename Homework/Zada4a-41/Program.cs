bef9ee5ad650d6c5d7c2efe4003deb3af37a3b33
immortalstr <immortalstr1990@mail.ru>
2022-12-01 12:35:46 +0300

    Modific 41 and 43

--- Homework/Zada4a-41/Program.cs
+++ Homework/Zada4a-41/Program.cs
@@ -1,4 +1,25 @@
 ﻿Console.Clear();
+
+Console.WriteLine($"Ввведите числа через запятую, например 0, 7, 8, -2, -2 ");
+string numbers = Console.ReadLine();
+
+string[] Separation(string num)
+{
+    char[] separator = { ',', ' ' }; //можно сделать через string[] separator = {", "}
+    string[] strlist = num.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+    return strlist;
+}
+
+int[] ConVertation(string[] strlist)
+{
+    int[] convertedarray = new int[strlist.Length];
+    for (int i = 0; i < strlist.Length; i++)
+    {
+        convertedarray[i] = Convert.ToInt32(strlist[i]);
+    }
+    return convertedarray;
+}
+
 void PrintArray(int[] array)
 {
     Console.Write($"[");
@@ -9,6 +30,29 @@ void PrintArray(int[] array)
     }
     Console.WriteLine($"]");
 }
+
+int PositiveNumbers(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) counter++;
+    }
+    return counter;
+}
+
+string[] StrNumbers = Separation(numbers);
+int[] array1 = ConVertation(StrNumbers);
+
+Separation(numbers);
+Console.WriteLine($"Числа преобразованны в массив");
+PrintArray(array1);
+Console.WriteLine($"Колличество чисел больше нуля = {PositiveNumbers(array1)}");
+
+// Console.WriteLine($"Ввведите колличество числе которые собираетесь вводить");
+// int length = Convert.ToInt32(Console.ReadLine());
+// int [] array1 = ReadNumbers(length);
+
 // int [] ReadNumbers(int size)
 // {
 //     int[] array2 = new int[size];
@@ -21,42 +65,5 @@ void PrintArray(int[] array)
 //     }
 //     return array2;
 // }
-int PositiveNumbers(int [] array)
-{
-    int counter = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i]  > 0) counter++;
-    }
-    return counter;
-}
 
-// Console.WriteLine($"Ввведите колличество числе которые собираетесь вводить");
-// int length = Convert.ToInt32(Console.ReadLine());
-// int [] array1 = ReadNumbers(length);
-
-Console.WriteLine($"Ввведите числа через запятую, например 0, 7, 8, -2, -2 ");
-string numbers = Console.ReadLine();
-
-string[] Separation(string num)
-{
-    string[] spearator = {", "};
-    string[] strlist = num.Split(spearator, StringSplitOptions.RemoveEmptyEntries);
-    return strlist;
-}
-int [] ConVertation(string [] strlist)
-{
-    int[] convertedarray = new int[strlist.Length];
-    for (int i = 0; i < strlist.Length; i++)
-    {
-        convertedarray[i] = Convert.ToInt32(strlist[i]);
-    }
-    return convertedarray;
-}
-string [] StrNumbers = Separation(numbers);
-int[] array1 = ConVertation(StrNumbers);
 // int[] array1 = Array.ConvertAll(StrNumbers, s => int.Parse(s));
-Separation(numbers);
-Console.WriteLine($"Числа преобразованны в массив");
-PrintArray(array1);
-Console.WriteLine($"Колличество чисел больше нуля = {PositiveNumbers(array1)}");
--- Homework/Zada4a-43/Program.cs
+++ Homework/Zada4a-43/Program.cs
@@ -12,15 +12,18 @@ double x2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Введите k2");
 double y2 = Convert.ToInt32(Console.ReadLine());
 
-double FindXPoint(double num1,double num2,double num3,double num4)
+double FindXPoint(double num1, double num2, double num3, double num4)
 {
-    double x = (-num3 + num1)/(-num2 + num4); 
+    double x = (-num3 + num1) / (-num2 + num4);
     return x;
 }
-double xresult = FindXPoint(x1, y1, x2, y2);
-double FindYPoint(double num1,double num2,double num3,double num4, double result)
+
+double FindYPoint(double num1, double num2, double num3, double num4, double result)
 {
     double y = num4 * result + num3;
     return y;
 }
+
+double xresult = FindXPoint(x1, y1, x2, y2);
+
 Console.WriteLine($"b1 = {x1}, k1 = {y1}, b2 = {x2}, k2 = {x2} -> ({FindXPoint(x1, y1, x2, y2)}; {FindYPoint(x1, y1, x2, y2, xresult)})");
