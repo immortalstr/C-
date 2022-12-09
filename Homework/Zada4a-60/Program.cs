14253c638ed49dd1200e989ba0a5384dbda9edf2
immortalstr <immortalstr1990@mail.ru>
2022-12-09 19:36:54 +0300

    Add and Finish 60 and 62

---
+++ Homework/Zada4a-60/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Например, задан массив размером 2 x 2 x 2.
+// Результат:
+// 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
+// 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
+
+int[,,] RandomMatrix(int rows, int columns, int depth, int min, int max)
+{
+    int[,,] matrix = new int[rows, columns, depth];
+    Random rnd = new Random();
+    matrix[0,0,0] = rnd.Next(min, max + 1);
+    int count = 1;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {   
+               if (matrix[i,j,k] == matrix[0,0,0]) count++;
+               if (count > 1) matrix[i,j,k] = rnd.Next(min, max + 1);
+            }
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,,] matrix, string sep1, string sep2)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write($"{sep1}");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                if (k < matrix.GetLength(2) - 0) Console.Write($"{matrix[i, j, k],5} ({i}, {j}, {k})|");
+                else Console.Write($"{matrix[i, j, k],5} ({i}, {j}, {k})");
+            }
+        }
+        Console.WriteLine($"{sep2}");
+    }
+}
+
+int[,,] array3D = RandomMatrix(2, 2, 2, 10, 99);
+PrintMatrix(array3D, "|", "|");
+Console.WriteLine("");
---
+++ Homework/Zada4a-62/Program.cs
@@ -0,0 +1,40 @@
+﻿int[,] RandomMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    int num = 1;
+    int i = 0;
+    int j = 0;
+    while (num <= rows * columns)
+    {
+        matrix[i, j] = num;
+        if (i <= j + 1 && i + j < rows - 1)
+            j++;
+        else if (i < j && i + j >= rows - 1)
+            i++;
+        else if (i >= j && i + j > rows - 1)
+            j--;
+        else
+            i--;
+        ++num;
+        
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix, string sep1, string sep2)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write($"{sep1}");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}|");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine($"{sep2}");
+    }
+}
+
+int [,] matrix = RandomMatrix(4,4);
+PrintMatrix(matrix,"[", "]");
+
